6ff23df5658ae9929ba5e84e8973a4584d5662f5
Lavikarawat <rawatlavi2005yahoo@gmail.com>
2025-05-04 12:30:17 +0530

    Update Regula.cpp

--- Regula.cpp
+++ Regula.cpp
@@ -1,51 +1,57 @@
-#include<iostream>
-#include<math.h>
-#include<iomanip>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
 using namespace std;
 
-float f(float x)
-{
-	return cos(x)-x*exp(x);
+double func(double x) {
+    return x * x * x - 2 * x - 5; // x^3 - 2x - 5
 }
-void regula (float *x, float x0,float x1, float fx0,float fx1, int *itr)
-{
-	*x=x0-((x1-x0)/(fx1-fx0))*fx0;
-	++(*itr);
-cout<<" Iteration No."<<setw(3)<<*itr<<"\t X= "<<setw(7)<<setprecision(5)<<*x<<endl;
 
-}
-int main()
-{
-	int itr=0, maxitr;
-	float x0,x1,x2,x3,aerr;
-	cout<<"\nEnter the value of x0: ";
-	cin>>x0;
-	cout<<"\nEnter the value of x1: ";
-	cin>>x1;
-	cout<<"\n enter allowed error: ";
-	cin>>aerr;
-	cout<<"\n maximum iterations: ";
-	cin>>maxitr;
-	cout<<endl;
-	regula(&x2,x0,x1,f(x0),f(x1),&itr);
-	do
-	{
-		if(f(x0)*f(x2)<0)
-		x1=x2;
-		else
-		x0=x2;
-		regula(&x3,x0,x1,f(x0),f(x1),&itr);
-		if(fabs(x3-x2)<aerr)
-		{
-		cout<<"After "<<itr<<" iterations, root = "<<x3<<endl;
-		return 0;
-		}
-		x2=x3;
-	}
-  while(itr<maxitr);
+int main() {
+    double a, b, c;
+    double fa, fb, fc;
+    double tolerance;
+    int maxIterations, iteration;
 
-    cout<<"Solution does not converge"<<endl;
-	cout<<"Iterations are not sufficient"<<endl;
-	return 1;
+    cout << "Enter the initial guess values a and b: ";
+    cin >> a >> b;
 
+    cout << "Enter the tolerance: ";
+    cin >> tolerance;
+
+    cout << "Enter the maximum number of iterations: ";
+    cin >> maxIterations;
+
+    fa = func(a);
+    fb = func(b);
+
+    if (fa * fb >= 0) {
+        cout << "The initial guesses do not bracket a root." << endl;
+        return 1;
+    }
+
+    cout << "Iteration\t a\t\t b\t\t c\t\t f(c)" << endl;
+
+    for (iteration = 1; iteration <= maxIterations; iteration++) {
+        c = (a * fb - b * fa) / (fb - fa);
+        fc = func(c);
+
+        cout << iteration << "\t\t" << fixed << setprecision(6) << a << "\t\t" << b << "\t\t" << c << "\t\t" << fc << endl;
+
+        if (abs(fc) < tolerance) {
+            cout << "Root found after " << iteration << " iterations: " << c << endl;
+            return 0;
+ 
+        if (fa * fc < 0) {
+            b = c;
+            fb = fc;
+        } else {
+            a = c;
+            fa = fc;
+        }
+    }
+
+    cout << "Root not found within the maximum number of iterations." << endl;
+    return 1;
 }
